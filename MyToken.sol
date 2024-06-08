5a560dc7fac3cee16a6a1b7df4ae0c7ff07ea3c6
Meenu Patel <149779716+Meenu00615@users.noreply.github.com>
2024-06-09 04:07:03 +0530

    Create MyToken.sol

---
+++ MyToken.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+contract MyToken {
+
+    // public variables here
+    string public tokenName = "Meenu";  //public string as variale name tokenName
+    string public tokenAbbrv = "Me";
+    uint public totalSupply = 0;
+
+    // mapping variable here
+    mapping(address => uint) public balances;
+
+    // mint function
+    function mint (address _address, uint _value) public{  //infinte gas
+        totalSupply += _value;  
+        balances[_address] += _value;
+    }
+
+    // burn function
+     function burn (address _address, uint _value) public{ //infinte gas
+         if (balances[_address] >= _value){
+        totalSupply -= _value;
+        balances[_address] -= _value;
+    }
+     }
+}
